668743262a7831cef8f9d889a652b3a3256fbbbf
rsshonjoydas <rsshonjoydas@gmail.com>
2021-07-24 21:11:43 +0600

    LInk Component Navigation

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
+import Link from 'next/link'
 
 const index = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <Link href="/blog">
+        <a>Blog</a>
+      </Link>
+      <Link href="/product">
+        <a>Products</a>
+      </Link>
     </div>
   )
 }
